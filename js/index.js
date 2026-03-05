ec70d446b4df31e3477a52c3aac626875830e790
NaimAli <owner.naimali@gmail.com>
2026-03-05 21:17:57 +0600

    dashboard update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 
-   // Initialize Icons
-    lucide.createIcons();
-    // Elements
-    const openBtn = document.getElementById('open-menu');
+   const openBtn = document.getElementById('open-menu');
     const closeBtn = document.getElementById('close-menu');
     const sidebar = document.getElementById('sidebar');
-    // Open Logic
+
+
+   // Initialize Icons
+try {
+  lucide.createIcons();
+} catch (e) {
+  console.log(e)
+}
+    
+    // Elements
+     // Open Logic
     function openMenu() {
       sidebar.classList.add('active');
       document.body.style.overflow = 'hidden'; // Prevents background scrolling
